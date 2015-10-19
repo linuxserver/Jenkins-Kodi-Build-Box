8614b8279f44663974cc579d658c9af6e3442a30
sparklyballs <sparklyballs@gmail.com>
2015-10-20 00:01:19 +0100

    Update 60_make_fpm.sh

--- docker/init/60_make_fpm.sh
+++ docker/init/60_make_fpm.sh
@@ -5,6 +5,6 @@ runtime_deps=`cat /defaults/rundeps/"$kodi_ver"/deps.list`
 
 cd /builds
 fpm -s dir -t deb -n kodi-headless -v "$kodi_version" -C /project \
-"$runtime_deps" .
+$runtime_deps .
 
 chown -R abc:abc /builds
