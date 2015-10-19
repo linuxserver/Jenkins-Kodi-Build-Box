0ab742530043d3395cb1273e652a650d9da0476b
sparklyballs <sparklyballs@gmail.com>
2015-10-19 15:59:33 +0100

    fix script that resulted in 15.2 deb error

--- docker/init/60_make_fpm.sh
+++ docker/init/60_make_fpm.sh
@@ -2,7 +2,7 @@
 
 kodi_ver="${kodi_version%.*}"
 
-. /docker/defaults/rundeps/"$kodi_ver"/deps.list
+. /defaults/rundeps/"$kodi_ver"/deps.list
 
 cd /builds
 fpm -s dir -t deb -n kodi-headless -v "$kodi_version" -C /project \
