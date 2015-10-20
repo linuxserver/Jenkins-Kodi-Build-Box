4e3ce734bf37a894f8ff2398db3c2f88dfd3c17f
sparklyballs <sparklyballs@gmail.com>
2015-10-20 06:49:02 +0100

    try yet another method to get fpm deps variable to work

--- docker/init/60_make_fpm.sh
+++ docker/init/60_make_fpm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 kodi_ver="${kodi_version%.*}"
-runtime_deps=`cat /defaults/rundeps/"$kodi_ver"/deps.list`
+runtime_deps=$(while read -r line; do echo -e "$line"; done < /defaults/rundeps/"$kodi_ver"/deps.list)
 
 cd /builds
 fpm -s dir -t deb -n kodi-headless -v "$kodi_version" -C /project \
-$runtime_deps .
+"$runtime_deps" .
 
 chown -R abc:abc /builds
