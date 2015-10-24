10fe8739d1ed400850ac7678584683d6020c2a42
sparklyballs <sparklyballs@gmail.com>
2015-10-24 07:02:51 +0100

    able to compile alpha version of Jarvis

--- docker/init/50_build_kodi.sh
+++ docker/init/50_build_kodi.sh
@@ -7,6 +7,14 @@ mkdir -p /project
 . /defaults/configures/"$kodi_ver"/config_options
 
 if [ "$kodi_ver" == "16" ]; then
+wget -O /tmp/libbluray.tar.bz2 ftp://ftp.videolan.org/pub/videolan/libbluray/0.9.0/libbluray-0.9.0.tar.bz2
+cd /tmp
+tar xvf libbluray.tar.bz2
+cd libbluray-*
+./bootstrap
+./configure --enable-bdjava
+make
+make install
 cd /source/kodi
 make -C tools/depends/target/crossguid PREFIX=/usr/local
 make -C tools/depends/target/libdcadec PREFIX=/usr/local
