be565ce0ef08f308752ca531cefeed928609cedd
sparklyballs <sparklyballs@gmail.com>
2015-10-24 05:21:12 +0100

    save up, many changes, mostly adding support 16 alpha

--- docker/init/30_get_source.sh
+++ docker/init/30_get_source.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-mkdir -p /source
+mkdir -p /source /build/source
 
 [[ -d /source/kodi/.git ]] && rm -rf /source/kodi
 
-git clone https://github.com/xbmc/xbmc.git /source/kodi &
+if [ ! -d "/builds/source/.git" ]; then
+git clone https://github.com/xbmc/xbmc.git /builds/source &
 pid="$!"
 wait "$pid"
+else
+cd /builds/source
+git pull
+fi
+
+cp -pr /builds/source /source/kodi
+
+chown -R abc:abc /builds
+
+
--- docker/init/50_build_kodi.sh
+++ docker/init/50_build_kodi.sh
@@ -6,6 +6,12 @@ mkdir -p /project
 
 . /defaults/configures/"$kodi_ver"/config_options
 
+if [ "$kodi_ver" == "16" ]; then
+cd /source/kodi
+make -C tools/depends/target/crossguid PREFIX=/usr/local
+make -C tools/depends/target/libdcadec PREFIX=/usr/local
+fi
+
 cd /source/kodi
 ./bootstrap
 ./configure \
