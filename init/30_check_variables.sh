e33c4967f8d9be13ca43c2ec71c7308082da70d2
sparklyballs <sparklyballs@gmail.com>
2016-02-02 10:23:25 +0000

    fix broken closing square bracket and separate tests instead of using or

--- init/30_check_variables.sh
+++ init/30_check_variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-[ "$KODI_VERSION" ] -o [ "$KODI_CHECKOUT" || (echo "you have not set KODI_VERSION or KODI_CHECKOUT"  && \ 
+[ "$KODI_VERSION" ] || (echo "you have not set KODI_VERSION"  && \
 exit 1)
+[ "$KODI_CHECKOUT" ] || (echo "you have not set KODI_CHECKOUT"  && \ 
+exit 1)
+
 
 
