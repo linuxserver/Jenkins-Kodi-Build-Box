2af040c99198bf3ea264f00cd029edba21a53c85
sparklyballs <sparklyballs@gmail.com>
2016-02-20 19:16:04 +0000

    Update 50_build_kodi.sh

--- init/50_build_kodi.sh
+++ init/50_build_kodi.sh
@@ -17,10 +17,8 @@ Config-Array "/defaults/configures/$kodi_ver/config_options"
 
 
 if [ "$kodi_ver" == "16" ]; then
-wget -O /tmp/libbluray.tar.bz2 ftp://ftp.videolan.org/pub/videolan/libbluray/0.9.0/libbluray-0.9.0.tar.bz2
-cd /tmp || exit
-tar xvf libbluray.tar.bz2
-cd libbluray-* || exit
+git clone http://git.videolan.org/git/libbluray.git /tmp/libbluray
+cd /tmp/libbluray || exit
 ./bootstrap
 ./configure --enable-bdjava
 make
